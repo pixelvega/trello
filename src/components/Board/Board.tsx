455bab83013c744a413044292f015e4dcf003910
pixelvega <pixelvega.info@gmail.com>
2021-09-26 02:55:22 +0200

    Feature: Move cards between columns

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -8,34 +8,53 @@ const Board = () => {
   const [data, setData] = useState<InitialData>(initialData);
 
   const onDragEnd = (result: any) => {
-    console.log(result)
     const { source, destination, draggableId } = result;
     if (!destination) {
       return;
     }
     if (source.droppableId === destination.droppableId &&
       source.index === destination.index) {
-      console.log('is the same place')
       return;
     }
-    if (destination.droppableId !== source.droppableId) {
-      // TO DO DRAG & DROP BETWEEN COLUMNS
-      return;
-    }
-    // delete dragableId from columna source.droppableId
-    // add dragableId in column source.droppableId
+
     let newData = { ...data };
-    let column = newData.columns.find((column: IColumn) => destination.droppableId === column.id)
-    let newCardsIds = column?.cardsIds;
-    newCardsIds?.splice(source.index, 1)
-    newCardsIds?.splice(destination.index, 0, draggableId)
-    const newColumn: any = {
-      ...column,
-      cardsIds: newCardsIds || []
+    let startColumn = newData.columns.find((column: IColumn) => source.droppableId === column.id);
+    const indexStartColumn = newData.columns.findIndex((item) => item.id === source.droppableId);
+    let endColumn = newData.columns.find((column: IColumn) => destination.droppableId === column.id)
+    const indexEndColumn = newData.columns.findIndex((item) => item.id === destination.droppableId);
+
+    // Moving from the same list
+    if (startColumn === endColumn) {
+      let newCardsIds = startColumn?.cardsIds;
+      newCardsIds?.splice(source.index, 1)
+      newCardsIds?.splice(destination.index, 0, draggableId)
+      const newColumn: any = {
+        ...startColumn,
+        cardsIds: newCardsIds || []
+      }
+      const indexOfColumn = newData.columns.findIndex((column) => column.id === destination.droppableId);
+      newData.columns.splice(indexOfColumn, 1, newColumn)
+      setData(newData);
+      return;
     }
-    const indexOfColumn = newData.columns.findIndex((column) => column.id === destination.droppableId);
-    newData.columns.splice(indexOfColumn, 1, newColumn)
+    // Moving between columns
+    const startCardsIds = Array.from(startColumn?.cardsIds || [])
+    startCardsIds.splice(source.index, 1)
+    const newStartColumn: any = {
+      ...startColumn,
+      cardsIds: startCardsIds
+    }
+    newData.columns.splice(indexStartColumn, 1, newStartColumn)
+
+    const endCardsIds = Array.from(endColumn?.cardsIds || []);
+    endCardsIds?.splice(destination.index, 0, draggableId)
+    const newEndColumn: any = {
+      ...endColumn,
+      cardsIds: endCardsIds
+    }
+    newData.columns.splice(indexEndColumn, 1, newEndColumn)
     setData(newData);
+
   }
 
   return (
@@ -47,7 +66,6 @@ const Board = () => {
         >
           <>
             {data.columns?.map((column: IColumn) => {
-
               const cardsArr: ICard[] | any = column.cardsIds.map((cardId) => {
                 const card = data.cards.find((card) => card.id === cardId)
                 if (!card) {
